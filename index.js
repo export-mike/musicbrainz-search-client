c2565ada157a6c03b2db075d21d37e61e24bf080
mike james <m.l.james@me.com>
2015-02-07 23:11:33 +0000

    init - requesting mb server and returning response parsed

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var request = require('request');
+var parseXmlString = require('xml2js').parseString;
+
+request('http://mb.trackstack.co/ws/2/recording?query=wonderwall', function(err, response, body){
+	if(err){
+		console.log(err);	
+		return;
+	} 
+
+	if(response.statusCode === 200){
+		parseXmlString(body, function(err, parsedBody){
+			if(err){
+				console.log(err);
+				return;
+			}
+
+			console.log(parsedBody);
+		});
+	}
+});
