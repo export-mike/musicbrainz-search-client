79a2dbbd200ce6d492eb8950717e0aa04c85fc0a
mike james <m.l.james@me.com>
2015-02-08 10:25:26 +0000

    refactoring request service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var requestService = require('./lib/requestService');
 
-requestService.get({uri:'http://mb.trackstack.co/ws/2/recording?query=wonderwall'}, function (err, result) {
+requestService.get({uri:'http://mb.trackstack.co/ws/2/recording?query=wonderwall\!'}, function (err, result) {
 	console.log('ERROR: '+ err);
 	console.log('RESULT: ' + result);
 });
