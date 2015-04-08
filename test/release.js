73ed6779c6497dcf362e81bf609ed203d672a0f4
mike james <m.l.james@me.com>
2015-04-08 21:52:47 +0100

    added Release.js

---
+++ test/release.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Release = require('../lib/release');
+
+describe('Release Url Construction', function() {
+
+	it('BaseUrl Matches one provided in constructor', function() {
+		var release = new Release({
+			searchServer: 'http://search.domain.com'
+		});
+
+		expect(release.baseUrl).to.equal('http://search.domain.com/ws/2/release/');
+	});
+
+	it('BaseUrl Matches default', function() {
+		var release = new Release();
+
+		expect(release.baseUrl).to.equal('https://search.musicbrainz.org/ws/2/release/');
+	});
+
+	it('Query has artist parameters', function() {
+		var release = new Release();
+
+
+		expect(release._createQueryValue({
+			artist: 'Jack Johnson'
+		})).to.equal('artist:"Jack Johnson"');
+
+	});
+
+	it('Query has artist and recording parameters', function() {
+		var release = new Release();
+		
+		expect(release._createQueryValue({
+			artist: 'Jack Johnson',
+			recording: 'breakdown'
+		})).to.equal('artist:"Jack Johnson" AND recording:"breakdown"');
+
+	});
+
+	it('Create full Url', function(){
+		var release = new Release({max:2, format:'xml'});
+
+		release._createQueryValue({
+			artist: 'Jack Johnson!',
+			recording: 'breakdown'
+		});
+
+		expect(release._createFullUrl()).
+		to.equal('https://search.musicbrainz.org/ws/2/release/?max=2&type=release&fmt=xml&offset=0&query=artist%3A%22Jack%20Johnson!%22%20AND%20recording%3A%22breakdown%22');
+	});
+});
